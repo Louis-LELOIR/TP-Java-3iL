3207c8cd4cbd2d238f381db2d303a327d18e50bb
Louis PLAQUET <58370603+Louis-PLAQUET@users.noreply.github.com>
2022-11-04 15:01:24 +0000

    ex4

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scanner = new Scanner (System.in);
+    System.out.println("Entrez un float : ");
+    if (scanner.hasNextDouble()) {
+      double d = scanner.nextDouble();
+      System.out.println(d + Double.MIN_NORMAL);
+      System.out.println(d - Double.MIN_NORMAL);
+    }
+    scanner.close();
   }
 }
