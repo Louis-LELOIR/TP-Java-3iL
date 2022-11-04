b75787c507c7009ec70ade64b2998b671b6ade0c
Louis PLAQUET <58370603+Louis-PLAQUET@users.noreply.github.com>
2022-11-04 15:46:15 +0000

    ex36

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+import java.util.*;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String sentence = "Le chien dans la niche";
+    String [] tempArray = sentence.split(" ");
+
+    StringBuilder reversed = new StringBuilder();
+
+    for (int i = tempArray.length; i > 0; i--) {
+          reversed.append(tempArray[i - 1]).append(" ");
+    };
+        
+    String[] reversedArray = reversed.toString().split(" ");
+
+    for (String word : reversedArray) {
+      System.out.println(word);
+    }
   }
 }
