e29059a6125bea9334e749655273b46a2564183f
Louis PLAQUET <58370603+Louis-PLAQUET@users.noreply.github.com>
2022-11-04 14:46:38 +0000

    First impl.

--- Main.java
+++ Main.java
@@ -1,5 +1,34 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    /*
+    int stopRow = 9;
+    char[][] americanFlag = new char [15][50];
+
+    for(int i = 0 ; i < 15 ; i ++) {
+      for(int j = 0; j < 50; j++) {
+        if (j <= 5 && i < 9 ){
+          
+        }
+        if (j <= 5) {
+          americanFlag[i][j] = '*';
+        } else {
+          americanFlag[i][j] = '=';
+        }
+      }
+    }
+
+    //System.out.println(americanFLag);
+    for(int i = 0 ; i < 15 ; i ++) {
+      for(int j = 0; j < 50; j++) {
+        System.out.print(americanFlag[i][j]);
+      }
+    }
+    */
+    for (int i = 0; i <= (15); i++) {
+      if (i <=9 ) {
+        i % 2 ? System.out.print('*') : System.out.print('=');
+      }
+      System.out.print('=');
+    }
   }
 }
