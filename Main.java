04567a1306ef167798696dda410ef994b827290a
Louis PLAQUET <58370603+Louis-PLAQUET@users.noreply.github.com>
2022-11-04 14:06:42 +0000

    ex2

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scanner = new Scanner (System.in);
+    int nombre = 0;
+    nombre = scanner.nextInt();
+    for(int i = 1 ; i <= 10 ; i++){
+      System.out.println(nombre * i);
+    }
   }
 }
