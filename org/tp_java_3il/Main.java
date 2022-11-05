c12fc9306bdd4975cd11cd80649a2db50214a9b6
Louis PLAQUET <louis.plaquet@gmail.com>
2022-11-05 19:50:14 +0100

    Refactor

--- org/tp_java_3il/Main.java
+++ org/tp_java_3il/Main.java
@@ -1,36 +1,36 @@
+package org.tp_java_3il;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.HashSet;
 import java.util.Set;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println(ex38());
+    Logger logger = Logger.getLogger(Main.class.getName());
+    logger.log(Level.INFO, "ex38() method returned : {0}", ex38("aabbbccccdeeeee"));
   }
 
-  public static String ex38(){
-    String input = "aabbbccccdeeeee";
+  public static String ex38(String input){
     String[] arrayWithDuplicates = input.split("");
 
     Set<String> arrayOfUniqueValues = getUnique(arrayWithDuplicates);
 
-    String returnValue = "";
+    StringBuilder returnValue = new StringBuilder();
     for (String value : arrayOfUniqueValues) {
-      returnValue += value;
+      returnValue.append(value);
       int count = 0;
       for(int i = 0; i < arrayWithDuplicates.length; i++) {
         int idx = arrayWithDuplicates[i].indexOf(value);
-        while(idx != -1) {
-          count++;
-          idx = arrayWithDuplicates[i].indexOf(value, idx+value.length());
-        }
+        if (idx != -1) { count++;}
       }
-      returnValue += Integer.toString(count);
+      returnValue.append(Integer.toString(count));
     }
-    
-    return returnValue;
+
+    return returnValue.toString();
   }
 
-  static Set getUnique(String[] arr) {
-    Set<String> values = new HashSet();
+  static Set<String> getUnique(String[] arr) {
+    Set<String> values = new HashSet<>();
     for (int i = 0; i < arr.length; i++) {
      if (!values.contains(arr[i])) {
       values.add(arr[i]);
