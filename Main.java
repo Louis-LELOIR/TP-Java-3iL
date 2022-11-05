bd93321262d2c7ae1cff83620708b84d7c3ba22b
Louis PLAQUET <louis.plaquet@gmail.com>
2022-11-05 17:47:00 +0100

    Ex10

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println(ex10());
+  }
+
+  static int sumOfInputs = 0 ;
+  public static int ex10(){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Entrez un nombre entier :");
+    
+    String input = s.nextLine();
+    ArrayList<String> splitArrayInput = new ArrayList<>(Arrays.asList(input.split("")));
+    
+    splitArrayInput.forEach((value) -> sumOfInputs += Integer.parseInt(value));
+    s.close();
+    return sumOfInputs;
   }
 }
